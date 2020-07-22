eebf83de0aa1bf62de7fb0a22c9803e244b7c5ff
Vladymer Mendoza <mvladymer@gmail.com>
2020-07-22 14:15:01 +0800

    Sum of three

--- ConsoleCoding.java
+++ ConsoleCoding.java
@@ -20,6 +20,7 @@ public class ConsoleCoding{
 			System.out.println("[11] Integer to Roman.");
 			System.out.println("[12] Roman to Integer.");
 			System.out.println("[13] Longest Common Prefix.");
+			System.out.println("[14] Sum of Three.");
 
 			System.out.println("'Welcome', Please choose a item from the choices above : ");
 			Scanner sc = new Scanner(System.in);
@@ -68,6 +69,9 @@ public class ConsoleCoding{
 				case 13 : 
 						new LongestCommonPrefix();
 					break;  
+				case 14 : 
+						new SumOfThree();
+					break; 
 				default : 
 					System.out.println("Option not available");
 			}
